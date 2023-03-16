d4cffc106d1d31d3adbec6b09d38e1460fc3d961
ed-pudin <ayedl851@gmail.com>
2023-03-15 19:15:57 -0600

    Editar linkedin y areas de estudiante

---
+++ app/Http/Livewire/ProfileStudent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\student;
+
+class ProfileStudent extends Component
+{
+    public $student;
+    public $idStudent;
+    public $oldLinkedin;
+
+    public function render()
+    {
+        return view('livewire.profile-student');
+    }
+
+    public function saveEditing(){
+
+        $student = student::find($this->idStudent);
+        $student->linkedin = $this->oldLinkedin;
+
+        if($student->save()){
+            $this->emit('saveEditingSuccess');
+        }else{
+            $this->emit('saveEditingFail');
+        }
+    }
+
+    public function editLinkedin(){
+        $this->emit('editLinkedin');
+    }
+
+    public function stopEditing(){
+        $this->emit('stopEditing');
+    }
+}
